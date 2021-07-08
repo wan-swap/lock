20057d9923b312247fafc3e142079e442f174224
pepe0xpuck <danieljames.maxdev@gmail.com>
2021-07-08 18:14:11 +0800

    add

---
+++ connectors/injected--.ts
@@ -0,0 +1,25 @@
+import LockConnector from '../src/connector';
+
+export default class Connector extends LockConnector {
+  async connect() {
+    let provider;
+    if (window['ethereum']) {
+      provider = window['ethereum'];
+      try {
+        await window['ethereum'].request({ method: 'eth_requestAccounts' })
+      } catch (e) {
+        console.error(e);
+      }
+    } else if (window['web3']) {
+      provider = window['web3'].currentProvider;
+    }
+    return provider;
+  }
+
+  async isLoggedIn() {
+    if (!window['ethereum']) return false;
+    if (window['ethereum'].selectedAddress) return true;
+    await new Promise(r => setTimeout(r, 400));
+    return !!window['ethereum'].selectedAddress
+  }
+}
